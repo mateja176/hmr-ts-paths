eb1a2b36c724a5efdeafe263aaa211cbb5496e7a
Mateja Petrovic <ma.petrovic@levi9.com>
2019-05-17 09:55:33 +0200

    uses parcel
    parcel installs and resolves all absolute imports from node_modules

--- packages/my-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { capitalize } from 'format';
-import React from 'react';
-import './App.css';
-
-const App: React.FC = () => {
-  return <div className="App">{capitalize('react')}</div>;
-};
-
-export default App;
